2df36290e57bdea4f467681540e5a78266cfc233
devloper64 <mdmahfuj010@gmail.com>
2023-09-30 00:19:29 +0600

    up to date

--- src/EdokPrivacyPolicy.js
+++ src/EdokPrivacyPolicy.js
@@ -22,4 +22,4 @@ const EdokPrivacyPolicy = () => {
     );
 };
 
-export default SShPrivacyPolicy;
+export default EdokPrivacyPolicy;
--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SShPrivacyPolicy from "./SssTerminalPrivacyPolicy";
 import Home from "./home";
+import EdokPrivacyPolicy from "../EdokPrivacyPolicy";
 
 function App() {
     return (
@@ -8,7 +9,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/privacy-policy-ssh-terminal" element={<SShPrivacyPolicy />} />
-                <Route path="/privacy-policy-edok" element={<SShPrivacyPolicy />} />
+                <Route path="/privacy-policy-edok" element={<EdokPrivacyPolicy />} />
             </Routes>
         </Router>
     );
