86ecda9f85d02cf8442d5ec0ebec0d82354aaaf3
devloper64 <mdmahfuj010@gmail.com>
2023-09-29 22:29:14 +0600

    up to date

--- src/components/Countdown/Timer.js
+++ src/components/Countdown/Timer.js
@@ -43,7 +43,7 @@ class CountDown extends Component {
     }
   }
   componentDidMount() {
-    this.deadline = new Date("Sept 24, 2023 21:00:00").getTime();
+    this.deadline = new Date("May 1, 2024 21:00:00").getTime();
 
     this.x = setInterval(this.count, 1000);
   }
